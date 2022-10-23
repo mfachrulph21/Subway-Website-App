80749303991cb540471279c929f12fe65d1de0ce
mfachrulph21 <mfachrulph21@gmail.com>
2022-10-23 16:30:08 +0700

    done client-Admin

--- client-admin/src/components/RowItem.js
+++ client-admin/src/components/RowItem.js
@@ -1,74 +1,86 @@
 import { Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import { deleteItem, itemDetail } from "../store/ItemsActions";
 
 export default function RowItem(props) {
-  const navigate = useNavigate()
-  const {items, formType, setType, handleShow} = props
+  const navigate = useNavigate();
+  const { items, formType, setType, handleShow } = props;
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const formatPrice = (number) => {
     return new Intl.NumberFormat("id-ID", {
       style: "currency",
-      currency: "IDR"
+      currency: "IDR",
     }).format(number);
-  }
+  };
 
   const handleClickEdit = (e, id) => {
-    e.preventDefault()
-    dispatch(itemDetail(id))
-    navigate(`/editItem/${id}`)
-  }
-
+    e.preventDefault();
+    dispatch(itemDetail(id));
+    navigate(`/editItem/${id}`);
+  };
 
   const handleDelete = (id) => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       showDenyButton: true,
-      confirmButtonText: 'Yes',
+      confirmButtonText: "Yes",
       denyButtonText: `No`,
     }).then((result) => {
       if (result.isConfirmed) {
-        dispatch(deleteItem(id))
-        Swal.fire('Deleted!', '', 'success')
+        dispatch(deleteItem(id));
+        Swal.fire("Deleted!", "", "success");
       } else if (result.isDenied) {
-        Swal.fire('Changes are not saved')
+        Swal.fire("Changes are not saved");
       }
-    })
-  }
+    });
+  };
 
-    return (
-        <>
-        {items.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{item.name}</td>
-                <td>{item.Category.name}</td>
-                <td>{formatPrice(item.price)}</td>
-                <td>{item.User.username}</td>
-                <td>
-                  <img className="imgTable" src={item.imgUrl} />
-                </td>
-                <td>
-                  <Button className="ingredients-button" variant="success">
+  return (
+    <>
+      {items.map((item, index) => {
+        return (
+          <tr style={{textAlign:'center', verticalAlign:'middle'}} key={index}>
+            <td>{index + 1}</td>
+            <td>{item.name}</td>
+            <td>{item.Category.name}</td>
+            <td>{formatPrice(item.price)}</td>
+            <td>{item.User.username}</td>
+            <td>
+              <img className="imgTable" src={item.imgUrl} />
+            </td>
+            <td>
+              {/* <Button className="ingredients-button" variant="success">
                     show ingredients
-                  </Button>
-                </td>
-                <td>
-                  <Button className="edit-button" variant="primary" onClick={(e) => handleClickEdit(e, item.id)} >
-                    Edit
-                  </Button>
-                  <Button className="delete-button" variant="danger" onClick={() => handleDelete(item.id)} >
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </>
-    )
+                  </Button> */}
+              <ul>
+                {item.Ingredients?.map((ingredient, index) => {
+                  return <li key={index}>{ingredient.name}</li>;
+                })}
+              </ul>
+            </td>
+            <td>
+              <Button
+                className="edit-button"
+                variant="primary"
+                onClick={(e) => handleClickEdit(e, item.id)}
+              >
+                Edit
+              </Button>
+              <Button
+                className="delete-button"
+                variant="danger"
+                onClick={() => handleDelete(item.id)}
+              >
+                Delete
+              </Button>
+            </td>
+          </tr>
+        );
+      })}
+    </>
+  );
 }
